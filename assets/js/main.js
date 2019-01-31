734bec8bfde6e1cc233641b75351932d7a476445
FeliOdras <ariane.feldmann@odras.de>
2019-01-31 13:22:20 +0100

    Created error handling. Hide form on correct input, hide output area on incorrect input

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
 calculateTip = () => {
     const getUserCurrency = document.getElementById('userCurrency').value;
     const getUserAmount = parseInt(document.getElementById('userAmount').value);
+    try {
+        if (isNaN(getUserAmount)) throw "Your amount should be a number.";
+    } catch (error) {
+        alert(error);
+    }
+
     const getUserSatisfactionLevel = document.getElementById('userSatisfactionLevel').value;
 
     if (getUserSatisfactionLevel == 'excellent') {
@@ -25,15 +31,28 @@ calculateTip = () => {
     const tipCalculatedValue = getUserAmount * tipCalculationValue;
     const finalResult = getUserAmount + tipCalculatedValue;
 
-    try {
-        if (isNaN(getUserAmount)) throw "Your amount should be a number.";
-    } catch (error) {
-        alert(error);
-    }
 
     const finalOutput = `Your <strong>Amount</strong> is <span class="color-amount">${getUserAmount} ${getUserCurrency}.<br>
     Your satifaction level is ${getUserSatisfactionLevel}, so your <strong>Tip</strong> should be ${tipCalculationPercentage}%. This is <span class="color-tip">${tipCalculatedValue} ${getUserCurrency}.<br>
-    Your <strong>Total</strong> is <span class="color-total">${finalResult}.`
-    finalOutput
+    Your <strong>Total</strong> is <span class="color-total">${finalResult}.<br>`
+    if (isNaN(getUserAmount)) {
+        document.getElementById("userInterfaceForm").style.display = "block";
+        document.getElementById("messageOutput").style.display = "none";
+    } else {
+        document.getElementById('messageOutput').innerHTML = finalOutput;
+        document.getElementById("userInterfaceForm").style.display = "none";
+        document.getElementById("messageOutput").style.display = "block";
+        document.getElementById("reloadButton").style.display = "block";
+    }
 }
-calculateTip()
+
+reloadForm = () => {
+    document.getElementById("messageOutput").style.display = "none";
+    document.getElementById("userInterfaceForm").style.display = "block";
+    document.getElementById("reloadButton").style.display = "none";
+}
+
+hideOutputArea = () => {
+    document.getElementById("messageOutput").style.display = "none";
+    document.getElementById("reloadButton").style.display = "none";
+}
